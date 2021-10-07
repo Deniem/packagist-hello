6db21a2d75a15c69438bb38825b7dbc221caa806
deniem <denis.kondakov@cashberry.com.ua>
2021-10-07 23:11:28 +0300

    Packagist Hello-World

---
+++ src/SayHello.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Deniem\HelloWorld;
+
+/**
+ *
+ */
+class SayHello
+{
+    /**
+     * @return string
+     */
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
